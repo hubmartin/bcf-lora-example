10454b42fa714434dce9dbb333130de56520e2bf
hubmartin <hub.martin@gmail.com>
2018-04-05 21:39:04 +0200

    Add TXCO control pin on P8

--- app/application.c
+++ app/application.c
@@ -1,12 +1,18 @@
 #include <application.h>
 #include <bc_cmwx1zzabz.h>
+
 // LED instance
 bc_led_t led;
 
 // Button instance
 bc_button_t button;
 
+// IMPORTANT, FIRST SET LORA MODULE TO 9600 BAUD 
+// AT+UART=9600\r
 bc_cmwx1zzabz_t lora;
+
+// GPIO to enable LoRa TXCO
+bc_gpio_channel_t lora_gpio_TXCO = BC_GPIO_P8;
 
 void button_event_handler(bc_button_t *self, bc_button_event_t event, void *event_param)
 {
@@ -23,17 +29,19 @@ void lora_callback(bc_cmwx1zzabz_t *self, bc_cmwx1zzabz_event_t event, void *eve
 
     if(event == BC_CMWX1ZZABZ_EVENT_ERROR)
     {
-        bc_led_set_mode(&led, BC_LED_MODE_BLINK_FAST);
+        //bc_led_set_mode(&led, BC_LED_MODE_BLINK_FAST);
     }
 
     if(event == BC_CMWX1ZZABZ_EVENT_SEND_RF_FRAME_START)
     {
         bc_led_set_mode(&led, BC_LED_MODE_ON);
+        bc_gpio_set_output(lora_gpio_TXCO, 1);
     }
 
     if(event == BC_CMWX1ZZABZ_EVENT_SEND_RF_FRAME_DONE)
     {
         bc_led_set_mode(&led, BC_LED_MODE_OFF);
+        bc_gpio_set_output(lora_gpio_TXCO, 0);
     }
 }
 
@@ -48,8 +56,11 @@ void application_init(void)
     bc_button_set_event_handler(&button, button_event_handler, NULL);
 
     bc_cmwx1zzabz_init(&lora, BC_UART_UART1);
-    
     bc_cmwx1zzabz_set_event_handler(&lora, lora_callback, NULL);
+
+    bc_gpio_init(lora_gpio_TXCO);
+    bc_gpio_set_output(lora_gpio_TXCO, 0);
+    bc_gpio_set_mode(lora_gpio_TXCO, BC_GPIO_MODE_OUTPUT);
 }
 
 void application_task()
