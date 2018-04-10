afdfb1af6624c5a91c3d43d5ce9dae4b5a93abaf
hubmartin <hub.martin@gmail.com>
2018-04-10 14:31:48 +0200

    Configuration save, OTAA test

--- app/application.c
+++ app/application.c
@@ -27,18 +27,27 @@ void lora_callback(bc_cmwx1zzabz_t *self, bc_cmwx1zzabz_event_t event, void *eve
     volatile int a = 0;
     a++;
 
-    if(event == BC_CMWX1ZZABZ_EVENT_ERROR)
+    if (event == BC_CMWX1ZZABZ_EVENT_READY)
+    {
+        volatile char deveui[20];
+        volatile char devaddr[20];
+        bc_cmwx1zzabz_get_deveui(&lora, (char*)deveui);
+        bc_cmwx1zzabz_get_devaddr(&lora, (char*)devaddr);
+        deveui[0] = 0;
+    }
+
+    if (event == BC_CMWX1ZZABZ_EVENT_ERROR)
     {
         //bc_led_set_mode(&led, BC_LED_MODE_BLINK_FAST);
     }
 
-    if(event == BC_CMWX1ZZABZ_EVENT_SEND_RF_FRAME_START)
+    if (event == BC_CMWX1ZZABZ_EVENT_SEND_RF_FRAME_START)
     {
         bc_led_set_mode(&led, BC_LED_MODE_ON);
         bc_gpio_set_output(lora_gpio_TXCO, 1);
     }
 
-    if(event == BC_CMWX1ZZABZ_EVENT_SEND_RF_FRAME_DONE)
+    if (event == BC_CMWX1ZZABZ_EVENT_SEND_RF_FRAME_DONE)
     {
         bc_led_set_mode(&led, BC_LED_MODE_OFF);
         bc_gpio_set_output(lora_gpio_TXCO, 0);
@@ -57,6 +66,18 @@ void application_init(void)
 
     bc_cmwx1zzabz_init(&lora, BC_UART_UART1);
     bc_cmwx1zzabz_set_event_handler(&lora, lora_callback, NULL);
+    //bc_cmwx1zzabz_set_appkey(&lora, "4257FAD1D73C3B7D4C2EDBAE6EBDA740");
+    //bc_cmwx1zzabz_set_appskey(&lora, "4357FAD1D73C3B7D4C2EDBAE6EBDA740");
+    //bc_cmwx1zzabz_set_nwkskey(&lora, "4457FAD1D73C3B7D4C2EDBAE6EBDA740");
+
+    bc_cmwx1zzabz_set_band(&lora, BC_CMWX1ZZABZ_CONFIG_BAND_EU868);
+    bc_cmwx1zzabz_set_mode(&lora, BC_CMWX1ZZABZ_CONFIG_MODE_OTAA);
+
+    bc_cmwx1zzabz_join(&lora);
+
+    volatile char deveui[16];
+    bc_cmwx1zzabz_get_deveui(&lora, (char*)deveui);
+
 
     bc_gpio_init(lora_gpio_TXCO);
     bc_gpio_set_output(lora_gpio_TXCO, 0);
@@ -65,15 +86,16 @@ void application_init(void)
 
 void application_task()
 {
-    uint8_t buffer[] = {'A', 'B', 'C'};
+    
 
-    if(!bc_cmwx1zzabz_is_ready(&lora))
+    if (!bc_cmwx1zzabz_is_ready(&lora))
     {
         bc_scheduler_plan_current_relative(200);
         return;
     }
 
-    bc_cmwx1zzabz_send_rf_frame(&lora, buffer, sizeof(buffer) );
+    //uint8_t buffer[] = {'A', 'B', 'C'};
+    //bc_cmwx1zzabz_send_message(&lora, buffer, sizeof(buffer) );
 
     // do not plan this task anymore
 }
